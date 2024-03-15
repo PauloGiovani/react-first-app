4169ce1de16c452b3d2a45a7576d255e61c0ae3b
Paulo Giovani <pg_faria@yahoo.com.br>
2024-03-14 21:59:35 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// https://www.youtube.com/watch?v=b9eMGE7QtTk&t=2318s
+
+// Component Person
+const Person = (props) => {
+    return (
+        <>
+            <h1>Name: {props.name}</h1>
+            <h2>Last Name: {props.lastName}</h2>
+            <h2>Age: {props.age}</h2>
+        </>
+    );
+};
+
+// Component Button
+const Button = () => {
+
+    // use state to control de counter value when some button is pressed
+    const [counter, setCounter] = useState(0);
+
+    // use effect to be executed as soon as the page is loaded
+    useEffect(() => {
+        setCounter(0);
+    }, []);
+
+    return (
+        <>
+            <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+            <h1>{counter}</h1>
+            <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+        </>
+    );
+};
+
+// App
+const App = () => {
+
+    return (
+        <div className="App">
+            <Person name='Paulo' lastName='Giovani' age={47} />
+            <br/>
+            <Button />
+        </div>
+    );
+};
 
 export default App;
